a5600dd8c55e410907fe906f16e6903f6b9e29dc
MarcinZawada <max.telepchuk@gmail.com>
2018-01-11 19:31:04 +0100

    st

---
+++ src/main/Main.java
@@ -0,0 +1,64 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.sql.*;
+
+public class Main extends Application {
+    private Stage primaryStage;
+    private Pane layout;
+    private String errorMsg;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        this.primaryStage=primaryStage;
+        this.primaryStage.setTitle("Hello World");
+        showStartView();
+    }
+
+    public void logIn(){
+
+    }
+    public void showStartView() throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/startView.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void showHomeView() throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/homeView.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        Application.launch(args);
+        String username = "root";
+        String password = "00000000";
+        String conectionURL = "jdbc:mysql://localhost:3306";
+        Class.forName("com.mysql.jdbc.Driver");
+        try(Connection connection = DriverManager.getConnection(conectionURL, username, password);
+            Statement statement = connection.createStatement()){
+                //statement.executeUpdate("");
+                ResultSet resultSet = statement.executeQuery("Select * from lab3.ludzie");
+               /*while(resultSet.next()){
+                    System.out.println(resultSet.getInt(1));
+                }*/
+
+                PreparedStatement preparedStatement = connection.prepareStatement("select * from lab3.ludzie where PESEL = ?");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
