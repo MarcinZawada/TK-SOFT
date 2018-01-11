44f47b02bfa180f70734138169973570283304bf
MarcinZawada <max.telepchuk@gmail.com>
2018-01-11 21:22:18 +0100

    add homeWorker

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         this.primaryStage=primaryStage;
-        this.primaryStage.setTitle("Hello World");
+        this.primaryStage.setTitle("TK-Soft");
         showStartView();
     }
 
@@ -26,16 +26,23 @@ public class Main extends Application {
     }
     public void showStartView() throws Exception{
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("view/startViewWorker.fxml"));
+        loader.setLocation(Main.class.getResource("view/startView.fxml"));
         mainlayout = loader.load();
         Scene scene = new Scene(mainlayout);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public static void goHomeView() throws Exception{
+    public static void goHomeWorker() throws Exception{
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("view/homeViewWorker.fxml"));
+        loader.setLocation(Main.class.getResource("view/homeWorkerView.fxml"));
+        BorderPane homeBorderPane = loader.load();
+        mainlayout.setCenter(homeBorderPane);
+    }
+
+    public static void goHomeAdmin() throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/homeAdminView.fxml"));
         BorderPane homeBorderPane = loader.load();
         mainlayout.setCenter(homeBorderPane);
     }
@@ -47,15 +54,15 @@ public class Main extends Application {
         String password = "00000000";
         String conectionURL = "jdbc:mysql://localhost:3306";
         Class.forName("com.mysql.jdbc.Driver");
-        try(Connection connection = DriverManager.getConnection(conectionURL, username, password);
-            Statement statement = connection.createStatement()){
-                //statement.executeUpdate("");
+        try(Connection connection = (DriverManager.getConnection(conectionURL, username, password));
+           Statement statement = connection.createStatement()){
+            /*    //statement.executeUpdate("");
                 ResultSet resultSet = statement.executeQuery("Select * from lab3.ludzie");
-               /*while(resultSet.next()){
+               while(resultSet.next()){
                     System.out.println(resultSet.getInt(1));
-                }*/
+                }
 
-                PreparedStatement preparedStatement = connection.prepareStatement("select * from lab3.ludzie where PESEL = ?");
+                PreparedStatement preparedStatement = connection.prepareStatement("select * from lab3.ludzie where PESEL = ?");*/
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/main/view/startViewController.java
+++ src/main/view/startViewController.java
@@ -3,12 +3,23 @@ package main.view;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import main.Main;//kadhsj
+
+import java.awt.*;
 
 public class startViewController {
     private Main main;
+
+    @FXML
+    private CheckBox isAdmin;
+
     @FXML
     public void logIn(ActionEvent event) throws Exception {
-        main.goHomeView();
+        if (isAdmin.isSelected()) {
+            main.goHomeAdmin();
+        } else {
+            main.goHomeWorker();
+        }
     }
 }
